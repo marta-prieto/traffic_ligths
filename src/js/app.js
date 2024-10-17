29f956c1524d65ac70053d6b759dcedcf39b09ef
Marta Prieto <martaprietoluque@gmail.com>
2024-10-17 17:00:41 +0200

    Add unit test to the first code

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,29 @@
-function app(a, b) {
-    return a + b;
+function trafficStatus(road, n) {
+
+    //toda la cadena de caracteres que simulan la carretera (incluyendo semáforos, coche y asfalto, se separan en index)
+    let roadArray = road.split('');
+
+    //semáforos
+    let trafficLigths = [];
+
+    //objeto con los tiempos de cada luz de semáforo
+    let lightTrafficTime = {
+        'G' : 5,
+        'O' : 0,
+        'R' : 5
+    };
+
+   for (let i =0; i< roadArray.length; i++) {
+    if(roadArray[i] === "G" || roadArray[i] === "O" || roadArray[i] === "R"){
+        //nuevo array
+        trafficLigths.push({ 
+            position: i, 
+            state: roadArray[i],
+            time: lightTrafficTime[roadArray[i]]
+        }) 
+    }
+   }
+  return trafficLigths;
 }
 
-module.exports = app; 
+module.exports = trafficStatus; 
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,14 @@
-const startApp = require('../src/js/app');
+const trafficStatus = require('../src/js/app');
 
-test('suma 1 + 2 es un total de 3', () => {
-    expect(startApp(1, 2)).toBe(3); 
-  });
+
+
+test('check de estados', () => {
+    const road = "....G....O....R....";
+    const resultado = trafficStatus(road, 5);
+    expect(resultado).toEqual([
+    { position: 4, state: 'G', time: 5 },
+    { position: 9, state: 'O', time: 0 }, 
+    { position: 14, state: 'R', time: 5 }
+    ])
+});
+
